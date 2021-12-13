59e46cedaaef41e2b54c12ff80fb44104ac5f537
kirti-77 <56729704+kirti-77@users.noreply.github.com>
2021-12-13 23:15:56 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,22 @@
+#nclude<iostream>
+#nclude<omp.h>
+using namespace std:
+int main()
+{
+  int sec,n,item,total=0,nt;
+  vector<int> a;
+  cout<<" no of sections and items";
+  cin>> sec>>item;
+  n=sec* item;
+  arrsize(n);
+  for(int i=0;i<n;i++)
+    a[i]=rand()%50;
+  omp_set_threads(2)
+    #pragma omp parallel for{
+    for(int i=0;i<n;i++)}
+      #pragma omp critical
+      total=total+a[i]
+        nt=omp_get_num_threads();
+      cout<< total<<endl<<nt;
+}
+      
