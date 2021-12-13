7b2368f9d90845dd32cf0b172b3a142071b43806
kirti-77 <56729704+kirti-77@users.noreply.github.com>
2021-12-13 23:07:30 +0530

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<vector.h>
+#include<omp.h>
+using namespace std;
+int main()
+{
+  vector<vector<int>> A(N,vector<int>(N,0), Y(N,vecotr<int>(N,0));
+  int X=5;
+  for (int i=0;i<N;i++)
+    for(int j=0;j<N;j++)
+      A[i] [j] =rand() % 100;
+  double start,end;
+    start = omp_get_wtime();
+ #pragma omp parallel for
+     for(int i=0;i<N;i++)
+      for(int j=0;j<N;j++)
+        Y[i][j]=A[i][j] * X
+     end = omp_get_wtime();
+     cout<<"time="<<end-start;
+                        }
